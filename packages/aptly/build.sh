76506e06f85e2b8379ac0904ac32b5c18a68c013
Henrik Grimler <henrik@grimler.se>
2021-05-12 11:52:35 +0200

    aptly: fix build and install into TERMUX_PKG_MASSAGEDIR

--- packages/aptly/build.sh
+++ packages/aptly/build.sh
@@ -15,15 +15,19 @@ termux_step_make() {
 	cp -a "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/aptly-dev/aptly
 	cd "$GOPATH"/src/github.com/aptly-dev/aptly
 
-	make install VERSION=$TERMUX_PKG_VERSION
+	go mod init
+	go mod vendor
 }
 
 termux_step_make_install() {
+	cd "$GOPATH"/src/github.com/aptly-dev/aptly
+	make install VERSION=$TERMUX_PKG_VERSION
+
 	install -Dm700 \
 		"$GOPATH"/bin/${GOOS}_${GOARCH}/aptly \
-		"$TERMUX_PREFIX"/bin/aptly
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/bin/aptly
 
 	install -Dm600 \
 		"$TERMUX_PKG_SRCDIR"/man/aptly.1 \
-		"$TERMUX_PREFIX"/share/man/man1/aptly.1
+		"$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"/share/man/man1/aptly.1
 }
