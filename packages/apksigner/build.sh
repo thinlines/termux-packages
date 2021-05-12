c0daac23918dbc427d26ca4a3474da122f38bbb0
Henrik Grimler <henrik@grimler.se>
2021-05-12 11:52:25 +0200

    apksigner: install into TERMUX_PKG_MASSAGEDIR instead of PREFIX

--- packages/apksigner/build.sh
+++ packages/apksigner/build.sh
@@ -30,7 +30,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_make() {
-	mkdir -p $TERMUX_PREFIX/share/dex
+	mkdir -p $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/dex/
 	cp $ANDROID_HOME/build-tools/${TERMUX_PKG_VERSION}/lib/apksigner.jar "$TERMUX_PKG_SRCDIR"
 	SOURCEFILE="$TERMUX_PKG_SRCDIR/apksigner.jar"
 
@@ -63,11 +63,11 @@ termux_step_make_install() {
 	cd $TERMUX_PKG_TMPDIR
 	unzip $SOURCEFILE */*.txt
 	jar cf apksigner.jar classes.dex com/
-	mv apksigner.jar $TERMUX_PREFIX/share/dex/apksigner.jar
+	mv apksigner.jar $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/dex/apksigner.jar
 
-	echo '#!/bin/sh' > $TERMUX_PREFIX/bin/apksigner
-	echo "dalvikvm -Xcompiler-option --compiler-filter=speed -cp $TERMUX_PREFIX/share/dex/apksigner.jar com.android.apksigner.ApkSignerTool \$@" >> $TERMUX_PREFIX/bin/apksigner
-	chmod +x $TERMUX_PREFIX/bin/apksigner
+	echo '#!/bin/sh' > $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/apksigner
+	echo "dalvikvm -Xcompiler-option --compiler-filter=speed -cp $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/share/dex/apksigner.jar com.android.apksigner.ApkSignerTool \$@" >> $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/apksigner
+	chmod +x $TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/bin/apksigner
 }
 
 termux_step_create_debscripts() {
